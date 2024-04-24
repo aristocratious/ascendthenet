2be86a8f56f4ac325406a833d0f4abe7d90cf117
Zach <zach@dotbound.com>
2024-04-24 15:21:35 -0500

    added netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,15 +10,13 @@ import compress from '@playform/compress';
 import astrowind from './vendor/integration';
 import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './src/utils/frontmatter.mjs';
 import decapCmsOauth from "astro-decap-cms-oauth";
-import netlify from "@astrojs/netlify";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const hasExternalScripts = false;
 const whenExternalScripts = (items = []) => hasExternalScripts ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
 
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hyb',
   integrations: [tailwind({
     applyBaseStyles: false
   }), sitemap(), mdx(), icon({
@@ -58,6 +56,5 @@ export default defineConfig({
         '~': path.resolve(__dirname, './src')
       }
     }
-  },
-  adapter: netlify()
+  }
 });
